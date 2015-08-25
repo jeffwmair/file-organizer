fae8ace566a537384343d4c8033b5414e5e04497
Jeff Mair <jeffwmair@gmail.com>
2015-08-24 22:32:43 -0400

    worked out file dates

--- src/directory.h
+++ src/directory.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DIRNAME "./tempdir/file"
+#define DIRNAME "../tempdir/"
 
 int directory_exists(const char * dirname);
 void get_directory_filenames(const char * dirname, char filenames[1000][500], int * filecount);
--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "filereader.h"
 #include "directory.h"
+
+double days_old_date(time_t t);
 
 int main(int argc, char * argv[]) {
 
@@ -32,11 +37,41 @@ int main(int argc, char * argv[]) {
 	int numFiles = 0;
 	get_directory_filenames(dirname, filenames, &numFiles);
 
+
+	/* get current time */
+	time_t t_now = time(NULL);
+	struct tm tm = *localtime(&t_now);
+	int cur_year = tm.tm_year + 1900;
+	int cur_month = tm.tm_mon + 1;
+	int cur_mday = tm.tm_mday;
+	printf("NOW: Year:%d, Month:%d, Day:%d\n", cur_year, cur_month, cur_mday);
+	printf("********************************\n");
+
 	int i;
 	for (i = 0; i < numFiles; i++) {
-		printf("%s\n", filenames[i]);
+		struct stat attrib;
+		stat(filenames[i], &attrib);
+		char date[10];
+		time_t t_file = attrib.st_ctime;
+		struct tm tm_file = *localtime(&attrib.st_ctime);
+		printf("File info: %s: Yr:%d Mon:%d, MDay:%d. ", filenames[i], tm_file.tm_year+1900, tm_file.tm_mon+1, tm_file.tm_mday);
+		double days_old = days_old_date(t_file);
+		printf("File is %G days old.\n", days_old);
 	}
 
 	return 0;
 
+}
+
+/**
+ * Calculates how many days ago the given date is
+ */
+double days_old_date(time_t t) {
+	time_t t_now = time(NULL);
+	struct tm tm = *localtime(&t_now);
+	double seconds = difftime(t_now, t);
+	double minutes = seconds / 60.0;
+	double hours = minutes / 60.0;
+	double days = hours / 24.0;
+	return days;
 }
